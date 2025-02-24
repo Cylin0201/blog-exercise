8d8b7d0e0e7281b38618a86b06195ba9daca1fa5
cylin0201 <cylin0201@naver.com>
2025-02-24 12:39:43 +0900

    initialization

---
+++ src/main/java/cylin0201/blog_exercise/BlogExerciseApplication.java
@@ -0,0 +1,13 @@
+package cylin0201.blog_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cylin0201/blog_exercise/BlogExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package cylin0201.blog_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
